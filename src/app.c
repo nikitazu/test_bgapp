e3dfb83eee9b3569fd6f458bdcd66bf1a6b9a453
Nikita Zuev <nikitazu@gmail.com>
2017-03-14 12:37:28 +0300

    print in thread

--- src/app.c
+++ src/app.c
@@ -1,16 +1,39 @@
 #include <stdio.h>
+#include <string.h>
+#include <pthread.h>
 #include <unistd.h>
 
-int counter_val = 0;
 int counter_max = 60;
 
+struct runCounter_args {
+  char prefix;
+  int val;
+};
+
+void runCounter(struct runCounter_args *args);
+
 int main() {
-  while (counter_val < counter_max) {
-    printf("c = %d\n", counter_val);
+  pthread_t tid;
+  struct runCounter_args args = {
+    .prefix = 'a'
+  , .val = 0
+  };
+  int ptresult = pthread_create(&tid, NULL, ((void * (*)(void *))&runCounter), &args);
+  if (ptresult != 0) {
+    printf("ERROR: failed to create a working thread [%s]\n", strerror(ptresult));
+    return 1;
+  }
+  sleep(61);
+  return 0;
+}
+
+
+void runCounter(struct runCounter_args *args) {
+  while (args->val < counter_max) {
+    printf("%c: c = %d\n", args->prefix, args->val);
     fflush(stdout);
-    counter_val += 1;
+    args->val += 1;
     sleep(1);
   }
-  return 0;
 }
 
